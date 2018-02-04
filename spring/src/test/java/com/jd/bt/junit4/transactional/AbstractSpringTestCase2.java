83e087777b4f01a2612e43dfaaf9c56a5fe28a79
benjaminwhx <benjaminwhx@sina.com>
2018-02-04 18:53:52 +0800

    spring test

--- spring/src/test/java/com/jd/bt/junit4/transactional/AbstractSpringTestCase2.java
+++ spring/src/test/java/com/jd/bt/junit4/transactional/AbstractSpringTestCase2.java
@@ -1,6 +1,7 @@
 package com.jd.bt.junit4.transactional;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -10,12 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
  * User: 吴海旭
  * Date: 2016-11-26
  * Time: 下午5:39
- * 抽象出来的测试类（spring-test 4.2版本之前使用）
+ * 抽象出来的测试类（spring-test 4.2版本之后使用）
  */
 @ContextConfiguration(locations = {"classpath:applicationContext/applicationContext.xml", "classpath:testDao.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
+@Transactional(transactionManager = "transactionManager")
 // rollback为true测试的时候不会污染数据库
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
-public abstract class AbstractSpringTestCase {
+@Rollback(value = true)
+public abstract class AbstractSpringTestCase2 {
 }
